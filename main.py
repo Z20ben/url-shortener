77892c6763fe7795ca4039a96596910d6fa456ce
Fredika Benz <benedictbenz.kerja@gmail.com>
2024-08-04 19:08:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     os.mkdir('dist')
 
     with open('dist/CNAME', 'w') as f:
-        f.write('tshr.me')
+        f.write('fredika.web.id')
 
     for link in links:
         html_document = html.format(url=link['url'])
